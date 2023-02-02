331824abec65b06443c1c90911d8b7ffaaafd1ba
cocicka21 <parkour949352@gmail.com>
2023-02-02 17:52:16 +0300

    added home work task-3

---
+++ it/academy/course/cocicka/HomeTask_3.java
@@ -0,0 +1,18 @@
+package it.academy.course.cocicka;
+
+import java.util.Scanner;
+
+public class HomeTask_3 {
+    public static void main(String[] args) {
+        System.out.println("\nTask-3");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter a period of time in seconds");
+        int FullSec = in.nextInt();
+        int weeks = FullSec / 604800;
+        int days = FullSec / 86400 % 7;
+        int hours = FullSec/ 3600 % 24;
+        int minutes = FullSec/ 60 % 60;
+        int seconds = FullSec % 60;
+        System.out.printf("weeks = %s\ndays = %s\nhours = %s\nminutes = %s\nseconds = %s\n", weeks, days, hours, minutes, seconds);
+    }
+}
