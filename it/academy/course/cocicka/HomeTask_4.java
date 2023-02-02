bb8af164c5ada083fccd6beea4194c4f89c240c2
cocicka21 <parkour949352@gmail.com>
2023-02-02 19:32:45 +0300

    added homework tasks 1-5

---
+++ it/academy/course/cocicka/HomeTask_4.java
@@ -0,0 +1,75 @@
+package it.academy.course.cocicka;
+
+import java.util.Scanner;
+
+public class HomeTask_4 {
+    public static void main(String[] args) {
+        System.out.println("\nTask-4");
+        byte b = 12;
+        ByteConvert(b);
+
+        short s = 121;
+        ShortConvert(s);
+
+        int i = 1212;
+        IntConvert(i);
+
+        long l = 121212L;
+        LongConvert(l);
+
+        float f = 1.21f;
+        FloatConvert(f);
+
+        double d = 1.2121;
+
+        char c = 'a';
+        CharConvert(c);
+    }
+
+    static byte ByteConvert(byte x){
+        short q = (short) x;
+        System.out.println("byte to short = " + q);
+        int w = (int) x;
+        System.out.println("byte to int = " + w);
+        long e = (long) x;
+        System.out.println("byte to long = " + e);
+        return x;
+    }
+    static short ShortConvert(short x){
+        int q = (int) x;
+        System.out.println("short to int = " + q);
+        long w = (long) x;
+        System.out.println("short to long = " + w);
+        float e = (float) x;
+        System.out.println("short to float = " + e);
+        double r = (double) x;
+        System.out.println("short to double = " + r);
+        return x;
+    }
+    static int IntConvert(int x){
+        long q = (long) x;
+        System.out.println("int to long = " + q);
+        float w = (float) x;
+        System.out.println("int to float = " + w);
+        double e = (double) x;
+        System.out.println("int to double = " + e);
+        return x;
+    }
+    static long LongConvert(long x){
+        float q = (float) x;
+        System.out.println("long to float = " + q);
+        double w = (double) x;
+        System.out.println("long to double = " + w);
+        return x;
+    }
+    static char CharConvert(char x){
+        int q = (int) x;
+        System.out.println("char to int = " + q);
+        return x;
+    }
+    static float FloatConvert(float x){
+        double q = (double) x;
+        System.out.println("float to double = " + q);
+        return x;
+    }
+}
---
+++ it/academy/course/cocicka/HomeTask_5.java
@@ -0,0 +1,19 @@
+package it.academy.course.cocicka;
+
+import java.util.Scanner;
+
+public class HomeTask_5 {
+    public static void main(String[] args) {
+        System.out.println("\nTask-5");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter any number");
+        int ReNumber = in.nextInt();
+        if (ReNumber > 0 && ReNumber % 2 == 0) {
+            System.out.println("0");
+        } else if (ReNumber > 0 && ReNumber % 2 != 0) {
+            System.out.println(ReNumber % 2);
+        } else {
+            System.out.println("Number is 0 or less than 0");
+        }
+    }
+}
