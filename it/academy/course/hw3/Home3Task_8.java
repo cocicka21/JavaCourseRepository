ecf4183780cc3aaec7ea7d5be1ef197f4ee00ae8
cocicka21 <98037827+cocicka21@users.noreply.github.com>
2023-02-05 13:58:56 +0300

    Update Home3Task_8.java
    corrected hw3 task 8

--- it/academy/course/hw3/Home3Task_8.java
+++ it/academy/course/hw3/Home3Task_8.java
@@ -5,14 +5,14 @@ import java.util.Random;
 public class Home3Task_8 {
     public static void main(String[] args){
         Random rnd = new Random();
-        int WorkDaySec = rnd.nextInt(28800);
-        int WorkDayHour = WorkDaySec / 3600;
-        System.out.println("Seconds left until the end of the day " + WorkDaySec);
-        switch (WorkDayHour) {
+        int workDaySec = rnd.nextInt(28800);
+        int workDayHour = workDaySec / 3600;
+        System.out.println("Seconds left until the end of the day " + workDaySec);
+        switch (workDayHour) {
             case 0: System.out.println("Less than an hour left");
-            case 1: System.out.println(WorkDayHour + " hour left");
-            case 2, 3, 4, 5, 6, 7, 8: System.out.println(WorkDayHour + " hours left");
-            default: System.out.println(WorkDayHour + " hours left");
+            case 1: System.out.println(workDayHour + " hour left");
+            case 2, 3, 4, 5, 6, 7, 8: System.out.println(workDayHour + " hours left");
+            default: System.out.println(workDayHour + " hours left");
         }
     }
 }
