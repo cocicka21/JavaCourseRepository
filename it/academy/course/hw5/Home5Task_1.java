61bb1300bc7403860aac1b94deda605c97c9c4ac
cocicka21 <98037827+cocicka21@users.noreply.github.com>
2023-03-11 14:32:04 +0300

    Update Home5Task_1.java

--- it/academy/course/hw5/Home5Task_1.java
+++ it/academy/course/hw5/Home5Task_1.java
@@ -3,10 +3,10 @@ package it.academy.course.hw5;
 public class Home5Task_1 {
     public static void main(String[] args) {
         Person mattew = new Person("Mattew Pirson", 47);
-        mattew.moving();
+        mattew.move();
         mattew.talk();
         Person john = new Person();
-        john.moving();
+        john.move();
         john.talk();
     }
 }
